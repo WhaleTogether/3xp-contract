6e2903d95e8e52f00620744ef1b70c18367add28
w17a <>
2023-04-28 07:48:57 +0700

    [feat] referral

--- contracts/3xpSale.sol
+++ contracts/3xpSale.sol
@@ -110,14 +110,22 @@ contract __3XPSale is
     }
 
     // projectId -> saleId -> SaleConfig
-    mapping(uint256 => mapping(uint256 => SaleConfig)) private _saleConfig;
+    // mapping(uint256 => mapping(uint256 => SaleConfig)) private _saleConfig;
+    mapping(uint256 => SaleConfig) private _saleConfig;
+    uint256 constant PUBLIC_SALE_ID = 0; // public sale
 
     // projectId -> saleId -> address -> whitelisted
     mapping(uint256 => mapping(uint256 => mapping(address => WhitelistedUser)))
         private whitelisted;
 
-    // mapping(uint256 => mapping(address => bool)) public _addressExist;
-    // uint256 constant PUBLIC_SALE_ID = 0; // public sale
+    mapping(uint256 => mapping(address => bool)) public _addressExist;
+
+    struct Referral {
+        address referredBy;
+        uint256 referredAt;
+    }
+
+    mapping(address => Referral) public _referrals;
 
     struct Project {
         string name;
@@ -125,7 +133,6 @@ contract __3XPSale is
         string description;
         string website;
         string license;
-        string projectBaseURI;
         uint256 supply;
         uint256 maxSupply;
         uint256 devReserve;
@@ -169,7 +176,7 @@ contract __3XPSale is
     function updateProjectArtistName(
         uint256 _projectId,
         string memory _projectArtistName
-    ) public onlyUnlocked(_projectId) onlyArtistOrDev(_projectId) {
+    ) public onlyUnlocked(_projectId) onlyArtist(_projectId) {
         projects[_projectId].artist = _projectArtistName;
     }
 
@@ -190,7 +197,7 @@ contract __3XPSale is
     function updateProjectLicense(
         uint256 _projectId,
         string memory _projectLicense
-    ) public onlyUnlocked(_projectId) onlyArtistOrDev(_projectId) {
+    ) public onlyUnlocked(_projectId) onlyArtist(_projectId) {
         projects[_projectId].license = _projectLicense;
     }
 
@@ -204,7 +211,7 @@ contract __3XPSale is
     function updateProjectContractAddress(
         uint256 _projectId,
         address _contractAddress
-    ) public onlyUnlocked(_projectId) onlyArtistOrDev(_projectId) {
+    ) public onlyUnlocked(_projectId) onlyArtist(_projectId) {
         projects[_projectId].contractAddress = _contractAddress;
     }
 
@@ -230,27 +237,27 @@ contract __3XPSale is
         _;
     }
 
-    modifier onlyDev() {
-        require(isWhitelisted[msg.sender], "Only whitelisted");
-        _;
-    }
+    // modifier onlyDev() {
+    //     require(isWhitelisted[msg.sender], "Only whitelisted");
+    //     _;
+    // }
 
-    modifier onlyArtistOrDev(uint256 _projectId) {
-        require(
-            isWhitelisted[msg.sender] ||
-                msg.sender == projectIdToArtistAddress[_projectId],
-            "Only artist or whitelisted"
-        );
-        _;
-    }
+    // modifier onlyArtistOrDev(uint256 _projectId) {
+    //     require(
+    //         isWhitelisted[msg.sender] ||
+    //             msg.sender == projectIdToArtistAddress[_projectId],
+    //         "Only artist or whitelisted"
+    //     );
+    //     _;
+    // }
 
-    function addDev(address _address) external onlyOwner {
-        isWhitelisted[_address] = true;
-    }
+    // function addDev(address _address) external onlyOwner {
+    //     isWhitelisted[_address] = true;
+    // }
 
-    function removeDev(address _address) external onlyOwner {
-        isWhitelisted[_address] = false;
-    }
+    // function removeDev(address _address) external onlyOwner {
+    //     isWhitelisted[_address] = false;
+    // }
 
     function addProject(
         string memory _projectName,
@@ -259,7 +266,7 @@ contract __3XPSale is
         uint _maxSupply,
         uint _devReserve,
         uint _artistReserve
-    ) external onlyDev {
+    ) external onlyOwner {
         uint256 projectId = nextProjectId;
 
         projects[projectId].name = _projectName;
@@ -277,11 +284,11 @@ contract __3XPSale is
 
     function toggleProjectIsLocked(
         uint256 _projectId
-    ) external onlyDev onlyUnlocked(_projectId) {
+    ) external onlyOwner onlyUnlocked(_projectId) {
         projects[_projectId].locked = true;
     }
 
-    function toggleProjectIsActive(uint256 _projectId) external onlyDev {
+    function toggleProjectIsActive(uint256 _projectId) external onlyOwner {
         projects[_projectId].active = !projects[_projectId].active;
     }
 
@@ -325,175 +332,175 @@ contract __3XPSale is
     /* 
         MINT
     */
-    modifier canMint(
-        uint256 projectId,
-        uint256 saleId,
-        address to,
-        uint256 amount
-    ) {
-        _guardMint(projectId, amount);
-        unchecked {
-            require(
-                projects[_projectId].active ||
-                    msg.sender == projectIdToArtistAddress[_projectId],
-                "Project must exist and be active"
-            );
-            require(
-                !projects[_projectId].paused ||
-                    msg.sender == projectIdToArtistAddress[_projectId],
-                "Purchases are paused."
-            );
+    // modifier canMint(
+    //     uint256 projectId,
+    //     uint256 saleId,
+    //     address to,
+    //     uint256 amount
+    // ) {
+    //     _guardMint(projectId, amount);
+    //     unchecked {
+    //         require(
+    //             projects[_projectId].active ||
+    //                 msg.sender == projectIdToArtistAddress[_projectId],
+    //             "Project must exist and be active"
+    //         );
+    //         require(
+    //             !projects[_projectId].paused ||
+    //                 msg.sender == projectIdToArtistAddress[_projectId],
+    //             "Purchases are paused."
+    //         );
 
-            SaleConfig memory saleConfig = _saleConfig[projectId][saleId];
-            if (!saleConfig.enabled) {
-                revert __3XP__SaleNotEnabled();
-            }
-            if (amount > saleConfig.maxPerTransaction) {
-                revert __3XP__ExceedsMaxPerTransaction();
-            }
+    //         SaleConfig memory saleConfig = _saleConfig[projectId][saleId];
+    //         if (!saleConfig.enabled) {
+    //             revert __3XP__SaleNotEnabled();
+    //         }
+    //         if (amount > saleConfig.maxPerTransaction) {
+    //             revert __3XP__ExceedsMaxPerTransaction();
+    //         }
 
-            if (saleConfig.currency == Currency.ETH) {
-                if (msg.value < (amount * saleConfig.unitPrice)) {
-                    revert __3XP__ETHAmountIsNotSufficient();
-                }
-            }
+    //         if (saleConfig.currency == Currency.ETH) {
+    //             if (msg.value < (amount * saleConfig.unitPrice)) {
+    //                 revert __3XP__ETHAmountIsNotSufficient();
+    //             }
+    //         }
 
-            // if (saleConfig.currency == Currency.ERC20) {
-            //     IERC20 erc20 = IERC20(saleConfig.erc20Address);
-            //     if (
-            //         erc20.allowance(_msgSenderERC721A(), address(this)) <
-            //         (amount * saleConfig.unitPrice)
-            //     ) {
-            //         revert __3XP__ERC20AllowanceIsNotSufficient();
-            //     }
-            //     if (
-            //         erc20.balanceOf(_msgSenderERC721A()) <
-            //         (amount * saleConfig.unitPrice)
-            //     ) {
-            //         revert __3XP__ERC20BalanceIsNotSufficient();
-            //     }
-            //     erc20.transferFrom(
-            //         _msgSenderERC721A(),
-            //         _devMultiSigWallet,
-            //         amount * saleConfig.unitPrice
-            //     );
-            // }
+    //         // if (saleConfig.currency == Currency.ERC20) {
+    //         //     IERC20 erc20 = IERC20(saleConfig.erc20Address);
+    //         //     if (
+    //         //         erc20.allowance(_msgSenderERC721A(), address(this)) <
+    //         //         (amount * saleConfig.unitPrice)
+    //         //     ) {
+    //         //         revert __3XP__ERC20AllowanceIsNotSufficient();
+    //         //     }
+    //         //     if (
+    //         //         erc20.balanceOf(_msgSenderERC721A()) <
+    //         //         (amount * saleConfig.unitPrice)
+    //         //     ) {
+    //         //         revert __3XP__ERC20BalanceIsNotSufficient();
+    //         //     }
+    //         //     erc20.transferFrom(
+    //         //         _msgSenderERC721A(),
+    //         //         _devMultiSigWallet,
+    //         //         amount * saleConfig.unitPrice
+    //         //     );
+    //         // }
 
-            // if (saleConfig.currency == Currency.ERC1155) {
-            //     //
-            // }
+    //         // if (saleConfig.currency == Currency.ERC1155) {
+    //         //     //
+    //         // }
 
-            if (
-                saleId > 0 &&
-                (saleConfig.supply == 0 || saleConfig.supply - amount < 0)
-            ) {
-                revert __3XP__ExceedsMaxPerRound();
-            }
-        }
-        _;
-    }
+    //         if (
+    //             saleId > 0 &&
+    //             (saleConfig.supply == 0 || saleConfig.supply - amount < 0)
+    //         ) {
+    //             revert __3XP__ExceedsMaxPerRound();
+    //         }
+    //     }
+    //     _;
+    // }
 
-    function _guardMint(
-        uint256 projectId,
-        uint256 _amount
-    ) internal view virtual {
-        unchecked {
-            if (tx.origin != _msgSender()) {
-                revert __3XP__CallerNotUser();
-            }
+    // function _guardMint(
+    //     uint256 projectId,
+    //     uint256 _amount
+    // ) internal view virtual {
+    //     unchecked {
+    //         if (tx.origin != _msgSender()) {
+    //             revert __3XP__CallerNotUser();
+    //         }
 
-            if (
-                projects[_projectId].supply + amount >
-                projects[_projectId].maxSupply
-            ) {
-                revert __3XP__ExceedMaxSupply();
-            }
-        }
-    }
+    //         if (
+    //             projects[_projectId].supply + amount >
+    //             projects[_projectId].maxSupply
+    //         ) {
+    //             revert __3XP__ExceedMaxSupply();
+    //         }
+    //     }
+    // }
 
-    function devMintTo(
-        uint256 projectId,
-        uint256 amount,
-        address to
-    ) external onlyDev {
-        if (amount > projects[_projectId].devReserve) {
-            revert __3XP__ExceedsDevReserve();
-        }
-        // _guardMint(projectId, amount);
-        // _safeMint(to, amount);
-        projects[_projectId].devReserve -= amount;
-    }
+    // function devMintTo(
+    //     uint256 projectId,
+    //     uint256 amount,
+    //     address to
+    // ) external onlyDev {
+    //     if (amount > projects[_projectId].devReserve) {
+    //         revert __3XP__ExceedsDevReserve();
+    //     }
+    //     // _guardMint(projectId, amount);
+    //     // _safeMint(to, amount);
+    //     projects[_projectId].devReserve -= amount;
+    // }
 
-    function mint(
-        uint256 _projectId,
-        uint256 _saleId,
-        uint256 _amount
-    )
-        public
-        payable
-        canMint(_projectId, _saleId, _msgSender(), _amount)
-        returns (uint256)
-    {
-        projects[_projectId].supply += amount;
+    // function mint(
+    //     uint256 _projectId,
+    //     uint256 _saleId,
+    //     uint256 _amount
+    // )
+    //     public
+    //     payable
+    //     canMint(_projectId, _saleId, _msgSender(), _amount)
+    //     returns (uint256)
+    // {
+    //     projects[_projectId].supply += amount;
 
-        if (projects[_projectId].contractAddress == address(0)) {
-            revert __3XPMintFactoryNotSet();
-        }
-        __3XPMintFactory factory = __3XPMintFactory(
-            projects[_projectId].contractAddress
-        );
-        factory.mint(recipient, _amount);
+    //     if (projects[_projectId].contractAddress == address(0)) {
+    //         revert __3XPMintFactoryNotSet();
+    //     }
+    //     __3XPMintFactory factory = __3XPMintFactory(
+    //         projects[_projectId].contractAddress
+    //     );
+    //     factory.mint(recipient, _amount);
 
-        // emit Mint(recipient, tokenIdToBe, _projectId);
+    //     // emit Mint(recipient, tokenIdToBe, _projectId);
 
-        // return tokenIdToBe;
-    }
+    //     // return tokenIdToBe;
+    // }
 
-    function exclusiveMint(
-        uint256 saleId,
-        uint256 amount,
-        bytes calldata signature
-    ) external payable canMint(saleId, _msgSender(), amount) {
-        // _feedCarrots(0, adoptionPlan.carrotAmountRequiredPerAdoption * amount);
+    // function exclusiveMint(
+    //     uint256 saleId,
+    //     uint256 amount,
+    //     bytes calldata signature
+    // ) external payable canMint(saleId, _msgSender(), amount) {
+    //     // _feedCarrots(0, adoptionPlan.carrotAmountRequiredPerAdoption * amount);
 
-        if (!_verify(saleId, _hash(_msgSender(), saleId), signature)) {
-            revert __3XP__InvalidSig();
-        } // check if this is a correct WL address
+    //     if (!_verify(saleId, _hash(_msgSender(), saleId), signature)) {
+    //         revert __3XP__InvalidSig();
+    //     } // check if this is a correct WL address
 
-        if (!_addressExist[saleId][_msgSender()]) {
-            // After verify the signature - check if address is already exist yet then create one
-            setWhitelistUser(
-                saleId,
-                _msgSender(),
-                _saleConfig[saleId].maxPerWallet
-            );
-        }
+    //     if (!_addressExist[saleId][_msgSender()]) {
+    //         // After verify the signature - check if address is already exist yet then create one
+    //         setWhitelistUser(
+    //             saleId,
+    //             _msgSender(),
+    //             _saleConfig[saleId].maxPerWallet
+    //         );
+    //     }
 
-        if (amount > whitelisted[saleId][_msgSender()].mintAmount) {
-            revert __3XP__ExceedsMaxPerWallet();
-        }
+    //     if (amount > whitelisted[saleId][_msgSender()].mintAmount) {
+    //         revert __3XP__ExceedsMaxPerWallet();
+    //     }
 
-        if (whitelisted[saleId][_msgSender()].mintAmount <= 0) {
-            revert __3XP__ExceedsMaxPerWallet();
-        }
+    //     if (whitelisted[saleId][_msgSender()].mintAmount <= 0) {
+    //         revert __3XP__ExceedsMaxPerWallet();
+    //     }
 
-        whitelisted[saleId][_msgSender()].mintAmount -= amount;
+    //     whitelisted[saleId][_msgSender()].mintAmount -= amount;
 
-        uint256 startTokenId = _nextTokenId();
-        _safeMint(_msgSender(), amount);
+    //     uint256 startTokenId = _nextTokenId();
+    //     _safeMint(_msgSender(), amount);
 
-        _saleConfig[saleId].maxPerRound -= amount;
-    }
+    //     _saleConfig[saleId].maxPerRound -= amount;
+    // }
 
-    function publicMint(
-        uint256 amount
-    ) external payable canMint(PUBLIC_SALE_ID, _msgSender(), amount) {
-        // if (amount > adoptionPlan.fcfsSupply) {
-        //     revert __3XP__ExceedsFCFSSupply();
-        // }
-        // adoptionPlan.fcfsSupply -= uint16(amount);
-        // _safeMint(_msgSender(), amount);
-    }
+    // function publicMint(
+    //     uint256 amount
+    // ) external payable canMint(PUBLIC_SALE_ID, _msgSender(), amount) {
+    //     // if (amount > adoptionPlan.fcfsSupply) {
+    //     //     revert __3XP__ExceedsFCFSSupply();
+    //     // }
+    //     // adoptionPlan.fcfsSupply -= uint16(amount);
+    //     // _safeMint(_msgSender(), amount);
+    // }
 
     function getPublicSaleConfig() external view returns (SaleConfig memory) {
         return _saleConfig[PUBLIC_SALE_ID];
@@ -520,14 +527,16 @@ contract __3XPSale is
         uint256 maxPerWallet,
         uint256 maxPerTransaction,
         uint256 unitPrice,
-        address signerAddress,
-        uint256 maxPerRound
+        uint256 supply,
+        uint256 maxSupply,
+        address signerAddress
     ) public onlyOwner {
         _saleConfig[saleId].maxPerWallet = uint8(maxPerWallet);
         _saleConfig[saleId].maxPerTransaction = uint8(maxPerTransaction);
         _saleConfig[saleId].unitPrice = uint64(unitPrice);
+        _saleConfig[saleId].supply = uint64(supply);
+        _saleConfig[saleId].maxSupply = uint64(maxSupply);
         _saleConfig[saleId].signerAddress = signerAddress;
-        _saleConfig[saleId].maxPerRound = maxPerRound;
     }
 
     function setSaleStatus(uint256 saleId, bool enabled) external onlyOwner {
@@ -536,27 +545,27 @@ contract __3XPSale is
         }
     }
 
-    function isWhitelisted(
-        uint256 saleId,
-        bytes calldata signature
-    ) public view returns (bool, uint256) {
-        // check if this address is whitelisted or not
-        uint256 mintAmount = 0;
-        bool isWhitelistedBool;
+    // function isWhitelisted(
+    //     uint256 saleId,
+    //     bytes calldata signature
+    // ) public view returns (bool, uint256) {
+    //     // check if this address is whitelisted or not
+    //     uint256 mintAmount = 0;
+    //     bool isWhitelistedBool;
 
-        if (_verify(saleId, _hash(_msgSender(), saleId), signature)) {
-            isWhitelistedBool = true;
-            if (!_addressExist[saleId][_msgSender()]) {
-                // After verify the signature - check if address is already exist yet then create one
-                mintAmount = _saleConfig[saleId].maxPerWallet;
-            } else {
-                mintAmount = whitelisted[saleId][_msgSender()].mintAmount;
-            }
-        } else {
-            isWhitelistedBool = false;
-        }
-        return (isWhitelistedBool, mintAmount);
-    }
+    //     if (_verify(saleId, _hash(_msgSender(), saleId), signature)) {
+    //         isWhitelistedBool = true;
+    //         if (!_addressExist[saleId][_msgSender()]) {
+    //             // After verify the signature - check if address is already exist yet then create one
+    //             mintAmount = _saleConfig[saleId].maxPerWallet;
+    //         } else {
+    //             mintAmount = whitelisted[saleId][_msgSender()].mintAmount;
+    //         }
+    //     } else {
+    //         isWhitelistedBool = false;
+    //     }
+    //     return (isWhitelistedBool, mintAmount);
+    // }
 
     function _hash(
         address account,
@@ -578,14 +587,22 @@ contract __3XPSale is
             ECDSAUpgradeable.recover(digest, signature);
     }
 
-    function setWhitelistUser(
-        uint256 saleId,
-        address _walletAddress,
-        uint256 _mintAmount
-    ) private {
-        whitelisted[saleId][_walletAddress].walletAddress = _walletAddress;
-        whitelisted[saleId][_walletAddress].mintAmount = _mintAmount;
-        _addressExist[saleId][_walletAddress] = true; // winner address;
+    // function setWhitelistUser(
+    //     uint256 saleId,
+    //     address _walletAddress,
+    //     uint256 _mintAmount
+    // ) private {
+    //     whitelisted[saleId][_walletAddress].walletAddress = _walletAddress;
+    //     whitelisted[saleId][_walletAddress].mintAmount = _mintAmount;
+    //     _addressExist[saleId][_walletAddress] = true; // winner address;
+    // }
+
+   function recordReferral(address referredBy) public {
+        require(_referrals[msg.sender].referredBy == address(0), "You have already been referred.");
+        _referrals[msg.sender] = Referral({
+            referredBy: referredBy,
+            referredAt: block.timestamp
+        });
     }
 
     /* 
